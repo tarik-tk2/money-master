2c278cf55d1cda5fe951efa25fabe27cec4ce879
tarik <mahimtk2@gmail.com>
2023-06-01 11:58:57 +0600

    calculation

--- source/script.js
+++ source/script.js
@@ -8,65 +8,84 @@ const percentageInput = document.getElementById("percentage-id");
 const percentageButton = document.getElementById("percentage-button");
 const saveAmount = document.getElementById("save-amount");
 const remainingAmount = document.getElementById("remaining-amount");
+let error = document.getElementById("errorId");
+
 
 function parseValue(id) {
   const inputValue = id.value;
   if (!isNaN(inputValue)) {
     return parseFloat(inputValue);
   } else {
-    console.log("Invalid Input");
+      error.textContent = "input is not a number";
+     
   }
 }
 function calculateExpense() {
-    const incomeValue=parseValue(income)
+  const incomeValue = parseValue(income);
   const foodValue = parseValue(foodInput);
   const rentValue = parseValue(rentInput);
-    const clothsValue = parseValue(clothsInput);
-    if (isNaN(incomeValue)||incomeValue==" ") {
-            return 'Invalid value '
-    } else {
-          const totalExpense = foodValue + rentValue + clothsValue;
-          return totalExpense;
-     }
-
+  const clothsValue = parseValue(clothsInput);
+  if (isNaN(incomeValue) || incomeValue == " ") {
+    error.textContent= "invalid income  value";
+  } else {
+    const totalExpense = foodValue + rentValue + clothsValue;
+    return totalExpense;
+  }
 }
 function remainBalance(balance, expense) {
   const remain = balance - expense;
   return remain;
 }
 function percentageCalculate(balance, percentage) {
-    if (percentage == !" ") {
-        const calculatePercentage = (percentage / 100) * balance;
-        return calculatePercentage;
-    } else { 
-        console.log("Percent invalid input");
-    }
- 
+
+    const calculatePercentage = (percentage / 100) * balance;
+    return calculatePercentage;
+  
 }
 document.getElementById("calculate-id").addEventListener("click", () => {
-  const expense = calculateExpense();
-  totalExpense.innerText = expense;
+    error.textContent=''
+    const expense = calculateExpense();
+      totalExpense.innerText = expense;
   const incomeInput = parseValue(income);
   const remain = remainBalance(incomeInput, expense);
-    if (isNaN(incomeInput) || isNaN(remain)||remain===" "||incomeInput===" ") {
-        console.log("Invalid input")
-    } else {
-      balanceRemain.innerText = remain;
-      income.value = " ";
-      foodInput.value = " ";
-      rentInput.value = " ";
-      clothsInput.value = " ";
-    }
+  if (
+    isNaN(incomeInput) ||
+    isNaN(remain) ||
+    remain === " " ||
+    incomeInput === " "
+  ) {
+    error.textContent="Invalid input";
+  } else {
+      
 
+    balanceRemain.innerText = remain;
+    income.value = " ";
+    foodInput.value = " ";
+    rentInput.value = " ";
+    clothsInput.value = " ";
+  }
 });
 
 percentageButton.addEventListener("click", () => {
-  const remainBalance = balanceRemain.innerText;
-  const percentageValue = parseValue(percentageInput);
-  const percentage = percentageCalculate(
-    parseInt(remainBalance),percentageValue
-    );
-   
-  saveAmount.innerText = percentage;
-  remainingAmount.innerText = remainBalance - percentage;
+  let percentageValue = parseValue(percentageInput);
+  const remainBalance = parseFloat(balanceRemain.innerText);
+    if (percentageValue === " "|| isNaN(percentageValue)) {
+        error.textContent = "input is not valid ";
+    } else { 
+
+        
+        if (calculateExpense() === "Invalid value") {
+            error.textContent = "please calculate first";
+        } else {
+          const percentage = percentageCalculate(
+            parseInt(remainBalance),
+            percentageValue
+          );
+          saveAmount.innerHTML = percentage;
+          remainingAmount.innerHTML = remainBalance - percentage;
+          percentageInput.value = " ";
+        }
+        }
+
+  
 });
